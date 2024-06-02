636bc8edb9a50bcbcdbe07e34a2e2e54266af106
Josue Cano <jdevelopcano@gmail.com>
2024-06-02 00:51:55 -0600

    finished menu

--- src/app/maps/layout/maps-layout/maps-layout.component.ts
+++ src/app/maps/layout/maps-layout/maps-layout.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   templateUrl: './maps-layout.component.html',
   styleUrl: './maps-layout.component.css',
+  host: { hostID: crypto.randomUUID().toString() },
 })
 export class MapsLayoutComponent {}
--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './full-screen-page.component.html',
-  styleUrl: './full-screen-page.component.css'
+  styleUrl: './full-screen-page.component.css',
+  host: { hostID: crypto.randomUUID().toString() },
 })
-export class FullScreenPageComponent {
-
-}
+export class FullScreenPageComponent {}
